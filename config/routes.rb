db302407a97f7aab928c31997479912d7d1debe9
Mohamed072005 <amine072005@gmail.com>
2025-03-17 11:02:07 +0000

    create the readme file

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Like routes
-  get 'likes/add_like', to: 'like#addLike', as: :addLike
-  delete 'likes/remove_like', to: 'like#removeLike', as: :removeLike
-
   # Authentication routes
   post 'auth/login', to: 'auth#login'
   post 'auth/register', to: 'auth#register'
